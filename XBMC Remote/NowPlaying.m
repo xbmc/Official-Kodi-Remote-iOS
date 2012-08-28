b1219ed74d74f248a7277cc37648b2660693e178
Giovanni Messina <joethefox@joelapmac.wag320n>
2012-08-28 10:00:28 +0200

    removed unused sendXbmcHttp method

--- XBMC Remote/NowPlaying.m
+++ XBMC Remote/NowPlaying.m
@@ -277,19 +277,6 @@ float cellBarWidth=45;
     [UIView commitAnimations];
 }
 
-#pragma mark - HTTP API managment
-
--(NSString *)sendXbmcHttp:(NSString *) command{
-    GlobalData *obj=[GlobalData getInstance]; 
-    NSString *userPassword=[obj.serverPass isEqualToString:@""] ? @"" : [NSString stringWithFormat:@":%@", obj.serverPass];
-    NSString *serverHTTP=[NSString stringWithFormat:@"http://%@%@@%@:%@/xbmcCmds/xbmcHttp?command=%@", obj.serverUser, userPassword, obj.serverIP, obj.serverPort, command];
-    NSURL *url = [NSURL  URLWithString:serverHTTP];
-    NSString *requestANS = [NSString stringWithContentsOfURL:url encoding:NSUTF8StringEncoding error:NULL]; 
-    return requestANS;
-//    requestANS=nil;
-    
-}
-
 #pragma  mark - JSON management
 
 int lastSelected=-1;
