e566e3facd08316a03a3803d19b35845a772ce77
joethefox <joethefox@gmail.com>
2012-11-14 19:36:49 +0100

    a new class to paint a gradient UIVIew

---
+++ XBMC Remote/gradientUIView.h
@@ -0,0 +1,13 @@
+//
+//  gradientUIView.h
+//  XBMC Remote
+//
+//  Created by Giovanni Messina on 14/11/12.
+//  Copyright (c) 2012 joethefox inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface gradientUIView : UIView
+
+@end
---
+++ XBMC Remote/gradientUIView.m
@@ -0,0 +1,55 @@
+//
+//  gradientUIView.m
+//  XBMC Remote
+//
+//  Created by Giovanni Messina on 14/11/12.
+//  Copyright (c) 2012 joethefox inc. All rights reserved.
+//
+
+#import "gradientUIView.h"
+
+@implementation gradientUIView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+- (void)drawRect:(CGRect)rect
+{
+    CGContextRef currentContext = UIGraphicsGetCurrentContext();
+    
+    CGGradientRef glossGradient;
+    CGColorSpaceRef rgbColorspace;
+    size_t num_locations = 2;
+    CGFloat locations[2] = { 0.0, 1.0 };
+    CGFloat components[8] = { 0.141, 0.141, 0.141, 1,  // Start color
+        0.07, 0.07, 0.07, 1 }; // End color
+    
+    rgbColorspace = CGColorSpaceCreateDeviceRGB();
+    glossGradient = CGGradientCreateWithColorComponents(rgbColorspace, components, locations, num_locations);
+    
+    CGRect currentBounds = self.bounds;
+    CGPoint topCenter = CGPointMake(CGRectGetMidX(currentBounds), 0.0f);
+    CGPoint midCenter = CGPointMake(CGRectGetMidX(currentBounds), CGRectGetMaxY(currentBounds));
+    CGContextDrawLinearGradient(currentContext, glossGradient, topCenter, midCenter, 0);
+    
+    CGGradientRelease(glossGradient);
+    CGColorSpaceRelease(rgbColorspace);
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
