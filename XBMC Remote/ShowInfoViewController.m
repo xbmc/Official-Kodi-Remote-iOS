d36ff37dc9bf150d353428e5f09c564dabb6d91a
joethefox <joethefox@gmail.com>
2013-03-02 14:37:48 +0100

    fix server version number check

--- XBMC Remote/ShowInfoViewController.m
+++ XBMC Remote/ShowInfoViewController.m
@@ -569,8 +569,8 @@ int h=0;
         CGRect frame;
         placeHolderImage = @"";
         if ([[item objectForKey:@"family"] isEqualToString:@"tvshowid"]){
-            GlobalData *obj=[GlobalData getInstance];     
-            if (obj.preferTVPosters==NO && [AppDelegate instance].serverVersion < 11){
+            GlobalData *obj=[GlobalData getInstance];
+            if (obj.preferTVPosters==NO && [AppDelegate instance].serverVersion < 12){
                 if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone){
                     coverHeight=70;
                     deltaY=coverView.frame.size.height - coverHeight;
