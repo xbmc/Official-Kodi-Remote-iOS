73c5be84913476c6be6e56c1832738e43fe56c7b
joethefox <joethefox@gmail.com>
2013-03-27 21:47:00 +0100

    collection view header fine tuning

--- XBMC Remote/PosterHeaderView.m
+++ XBMC Remote/PosterHeaderView.m
@@ -19,29 +19,35 @@
     self = [super initWithFrame:frame];
     if (self) {
         self.restorationIdentifier = @"posterHeaderView";
-        CAGradientLayer *gradient = [CAGradientLayer layer];
-        gradient.frame = self.bounds;
-        gradient.colors = [NSArray arrayWithObjects:(id)[[UIColor colorWithRed:.6 green:.6 blue:.6 alpha:.95] CGColor], (id)[[UIColor colorWithRed:.9 green:.9 blue:.9 alpha:.95] CGColor], nil];
-        [self.layer insertSublayer:gradient atIndex:0];
-
-        UIView *lineView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, 1)];
-        [lineView setBackgroundColor:[UIColor colorWithRed:130.0f/255.0f green:130.0f/255.0f blue:130.0f/255.0f alpha:1]];
-        [self addSubview:lineView];
         
-        UIView *lineViewBottom = [[UIView alloc] initWithFrame:CGRectMake(0, self.frame.size.height - 1, self.frame.size.width, 1)];
-        [lineViewBottom setBackgroundColor:[UIColor colorWithRed:154.0f/255.0f green:154.0f/255.0f blue:154.0f/255.0f alpha:1]];
-        [self addSubview:lineViewBottom];
+        if (self.frame.size.height > 0){
+            UIView *lineView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, 1)];
+            [lineView setBackgroundColor:[UIColor colorWithRed:130.0f/255.0f green:130.0f/255.0f blue:130.0f/255.0f alpha:1]];
+            [self addSubview:lineView];
+        }
+        
+        if (self.frame.size.height > 1){
+            UIView *lineViewBottom = [[UIView alloc] initWithFrame:CGRectMake(0, self.frame.size.height - 1, self.frame.size.width, 1)];
+            [lineViewBottom setBackgroundColor:[UIColor colorWithRed:154.0f/255.0f green:154.0f/255.0f blue:154.0f/255.0f alpha:1]];
+            [self addSubview:lineViewBottom];
+            CAGradientLayer *gradient = [CAGradientLayer layer];
+            gradient.frame = self.bounds;
+            gradient.colors = [NSArray arrayWithObjects:(id)[[UIColor colorWithRed:.6 green:.6 blue:.6 alpha:.95] CGColor], (id)[[UIColor colorWithRed:.9 green:.9 blue:.9 alpha:.95] CGColor], nil];
+            [self.layer insertSublayer:gradient atIndex:0];
+        }
 
-        _headerLabel = [[PosterLabel alloc] initWithFrame:CGRectMake(10, 0, self.frame.size.width - 10, self.frame.size.height - 1)];
-        [_headerLabel setBackgroundColor:[UIColor clearColor]];
-        [_headerLabel setFont:[UIFont boldSystemFontOfSize:17]];
-        [_headerLabel setShadowColor:[UIColor darkGrayColor]];
-        [_headerLabel setShadowOffset:CGSizeMake(0, 1)];
-
-        [_headerLabel setTextColor:[UIColor whiteColor]];
-        [_headerLabel setAutoresizingMask:UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin];
-
-        [self addSubview:_headerLabel];
+        if (self.frame.size.height > 10){
+            _headerLabel = [[PosterLabel alloc] initWithFrame:CGRectMake(10, 0, self.frame.size.width - 10, self.frame.size.height - 1)];
+            [_headerLabel setBackgroundColor:[UIColor clearColor]];
+            [_headerLabel setFont:[UIFont boldSystemFontOfSize:(self.frame.size.height > 20 ? 17 : self.frame.size.height - 2)]];
+            [_headerLabel setShadowColor:[UIColor darkGrayColor]];
+            [_headerLabel setShadowOffset:CGSizeMake(0, 1)];
+            
+            [_headerLabel setTextColor:[UIColor whiteColor]];
+            [_headerLabel setAutoresizingMask:UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin];
+            
+            [self addSubview:_headerLabel];
+        }
     }
     return self;
 }
