c6db2d12024a16f64d42d245b93f63af3f4b4f97
joethefox <joethefox@gmail.com>
2013-04-14 17:32:48 +0200

    check if the thumbnail colour space is supported

--- XBMC Remote/UIImage+Resize/UIImage+Resize.m
+++ XBMC Remote/UIImage+Resize/UIImage+Resize.m
@@ -18,13 +18,41 @@
     ratio = MAX(horizontalRatio, verticalRatio); //UIViewContentModeScaleAspectFill
     newSize = CGSizeMake(self.size.width * ratio, self.size.height * ratio);
     newRect = CGRectIntegral(CGRectMake(0, 0, newSize.width, newSize.height));
+    
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGBitmapInfo bitmapInfo = CGImageGetBitmapInfo(imageRef);
+    
+    int infoMask = (bitmapInfo & kCGBitmapAlphaInfoMask);
+    BOOL anyNonAlpha = (infoMask == kCGImageAlphaNone ||
+                        infoMask == kCGImageAlphaNoneSkipFirst ||
+                        infoMask == kCGImageAlphaNoneSkipLast);
+    
+    // CGBitmapContextCreate doesn't support kCGImageAlphaNone with RGB.
+    // https://developer.apple.com/library/mac/#qa/qa1037/_index.html
+    if (infoMask == kCGImageAlphaNone && CGColorSpaceGetNumberOfComponents(colorSpace) > 1)
+    {
+        // Unset the old alpha info.
+        bitmapInfo &= ~kCGBitmapAlphaInfoMask;
+        
+        // Set noneSkipFirst.
+        bitmapInfo |= kCGImageAlphaNoneSkipFirst;
+    }
+    // Some PNGs tell us they have alpha but only 3 components. Odd.
+    else if (!anyNonAlpha && CGColorSpaceGetNumberOfComponents(colorSpace) == 3)
+    {
+        // Unset the old alpha info.
+        bitmapInfo &= ~kCGBitmapAlphaInfoMask;
+        bitmapInfo |= kCGImageAlphaPremultipliedFirst;
+    }
+    
     CGContextRef bitmap = CGBitmapContextCreate(NULL,
                                                 newRect.size.width,
                                                 newRect.size.height,
                                                 CGImageGetBitsPerComponent(imageRef),
                                                 0,
-                                                CGImageGetColorSpace(imageRef),
-                                                CGImageGetBitmapInfo(imageRef));
+                                                colorSpace,
+                                                bitmapInfo);
+    CGColorSpaceRelease(colorSpace);
     CGContextSetInterpolationQuality(bitmap, quality);
     CGContextDrawImage(bitmap, newRect, imageRef);
     CGImageRef newImageRef = CGBitmapContextCreateImage(bitmap);
