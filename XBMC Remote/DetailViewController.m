31fb75d5cecc56c63f08a16dd6e6437aadf628ce
joethefox <joethefox@gmail.com>
2013-09-07 10:11:40 +0200

    init the table header view with an UISearchBar instead of an UIView

--- XBMC Remote/DetailViewController.m
+++ XBMC Remote/DetailViewController.m
@@ -488,7 +488,7 @@
         self.searchDisplayController.searchBar.tintColor = tableViewSearchBarColor;
         [self.searchDisplayController.searchBar setBackgroundColor:tableViewSearchBarColor];
         if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"7.0")){
-            dataList.tableHeaderView = [[UIView alloc] initWithFrame:self.searchDisplayController.searchBar.frame];
+            dataList.tableHeaderView = [[UISearchBar alloc] initWithFrame:self.searchDisplayController.searchBar.frame];
             self.searchDisplayController.searchBar.tintColor = [utils lighterColorForColor:tableViewSearchBarColor];
             [bar.viewLabel setTextColor:[UIColor colorWithRed:0.2f green:0.2f blue:0.2f alpha:0.65f]];
             [bar.viewLabel setShadowColor:[UIColor colorWithRed:0.9f green:0.9f blue:0.9f alpha:0.3f]];
