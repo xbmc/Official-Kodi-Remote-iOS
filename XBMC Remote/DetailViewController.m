27b748bc80e048dae9f6ea4de648800fc0c40d11
joethefox <joethefox@gmail.com>
2013-09-11 11:10:51 +0200

    hide the background search bar

--- XBMC Remote/DetailViewController.m
+++ XBMC Remote/DetailViewController.m
@@ -488,7 +488,9 @@
         self.searchDisplayController.searchBar.tintColor = tableViewSearchBarColor;
         [self.searchDisplayController.searchBar setBackgroundColor:tableViewSearchBarColor];
         if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"7.0")){
-            dataList.tableHeaderView = [[UISearchBar alloc] initWithFrame:self.searchDisplayController.searchBar.frame];
+            UISearchBar *hackSearchBar = [[UISearchBar alloc] initWithFrame:self.searchDisplayController.searchBar.frame];
+            hackSearchBar.hidden = YES;
+            dataList.tableHeaderView = hackSearchBar;
             self.searchDisplayController.searchBar.tintColor = [utils lighterColorForColor:tableViewSearchBarColor];
             [bar.viewLabel setTextColor:[UIColor colorWithRed:0.2f green:0.2f blue:0.2f alpha:0.65f]];
             [bar.viewLabel setShadowColor:[UIColor colorWithRed:0.9f green:0.9f blue:0.9f alpha:0.3f]];
@@ -2050,6 +2052,7 @@ int originYear = 0;
         }
         [collectionView addGestureRecognizer:longPressGesture];
     }
+    [self.searchDisplayController.searchBar layoutSubviews];
 }
 
 - (void) searchDisplayControllerWillEndSearch:(UISearchDisplayController *)controller{
