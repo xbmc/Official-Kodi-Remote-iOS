5848fef8084bb2275a57c70bfbe19c5c8ce60038
joethefox <joethefox@gmail.com>
2014-04-08 22:17:20 +0200

    darken footer background color

--- XBMC Remote/SettingsValuesViewController.m
+++ XBMC Remote/SettingsValuesViewController.m
@@ -633,7 +633,7 @@
         
         UIView *helpView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, footerHeight)];
         [helpView setBackgroundColor:[UIColor clearColor]];
-        [helpView setBackgroundColor:[UIColor colorWithRed:65.0f/255.0f green:65.0f/255.0f blue:65.0f/255.0f alpha:0.8f]];
+        [helpView setBackgroundColor:[UIColor colorWithRed:45.0f/255.0f green:45.0f/255.0f blue:45.0f/255.0f alpha:0.95f]];
         [helpView addSubview:descriptionLabel];
         return helpView;
     }
