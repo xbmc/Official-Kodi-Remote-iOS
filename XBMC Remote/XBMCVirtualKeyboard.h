53937b49bfb7693156f5c85061149cd50cf92fb8
joethefox <joethefox@gmail.com>
2017-02-26 18:09:48 +0100

    Use a real UITextField for XBMC Keyboard input accessory

--- XBMC Remote/XBMCVirtualKeyboard.h
+++ XBMC Remote/XBMCVirtualKeyboard.h
@@ -12,7 +12,6 @@
 @interface XBMCVirtualKeyboard : UIView <UITextFieldDelegate>{
     UITextField *xbmcVirtualKeyboard;
     UIView *inputAccView;
-    UILabel *verboseOutput;
     UILabel *keyboardTitle;
     UITextField *backgroundTextField;
     int accessoryHeight;
--- XBMC Remote/XBMCVirtualKeyboard.m
+++ XBMC Remote/XBMCVirtualKeyboard.m
@@ -29,7 +29,7 @@
             alignBottom = 12;
         }
 
-        xbmcVirtualKeyboard = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, 300, 20)];
+        xbmcVirtualKeyboard = [[UITextField alloc] initWithFrame:frame];
         xbmcVirtualKeyboard.hidden = YES;
         xbmcVirtualKeyboard.delegate = self;
         xbmcVirtualKeyboard.autocorrectionType = UITextAutocorrectionTypeNo;
@@ -40,37 +40,30 @@
         CGSize screenSize = screenBound.size;
         screenWidth = screenSize.width;
         
-        verboseOutput = [[UILabel alloc] initWithFrame:CGRectMake(padding, (int)(accessoryHeight/2) - (int)(verboseHeight/2) + alignBottom, screenWidth - padding * 2, verboseHeight)];
-        [verboseOutput setFont:[UIFont systemFontOfSize:textSize]];
-        [verboseOutput setContentMode:UIViewContentModeScaleToFill];
-        [verboseOutput setAutoresizingMask:UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleLeftMargin];
-        [verboseOutput setLineBreakMode:NSLineBreakByTruncatingHead];
-        [verboseOutput setUserInteractionEnabled:NO];
-        [verboseOutput setBackgroundColor:[UIColor clearColor]];
-        [verboseOutput setTextAlignment:NSTextAlignmentCenter];
-        
         keyboardTitle = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, screenWidth, (int)(accessoryHeight/2) - (int)(verboseHeight/2) + alignBottom + 1)];
         [keyboardTitle setContentMode:UIViewContentModeScaleToFill];
         [keyboardTitle setAutoresizingMask:UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleLeftMargin];
         [keyboardTitle setTextAlignment:NSTextAlignmentCenter];
         [keyboardTitle setBackgroundColor:[UIColor clearColor]];
         [keyboardTitle setFont:[UIFont boldSystemFontOfSize:textSize]];
-        
-        inputAccView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, screenWidth, accessoryHeight)];
-        [inputAccView setBackgroundColor:accessoryBackgroundColor];
-
         [keyboardTitle setTextColor:BAR_TINT_COLOR];
 
         backgroundTextField = [[UITextField alloc] initWithFrame:CGRectMake(padding - background_padding, (int)(accessoryHeight/2) - (int)(verboseHeight/2) + alignBottom, screenWidth - (padding - background_padding) * 2, verboseHeight)];
-        [backgroundTextField setUserInteractionEnabled:NO];
+        [backgroundTextField setUserInteractionEnabled:YES];
         [backgroundTextField setBorderStyle:UITextBorderStyleRoundedRect];
         [backgroundTextField setBackgroundColor:[UIColor whiteColor]];
-        [backgroundTextField setFont:[UIFont boldSystemFontOfSize:textSize]];
+        [backgroundTextField setFont:[UIFont systemFontOfSize:textSize]];
         [backgroundTextField setAutoresizingMask:UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleLeftMargin];
-
+        backgroundTextField.autocorrectionType = UITextAutocorrectionTypeNo;
+        backgroundTextField.autocapitalizationType = UITextAutocapitalizationTypeNone;
+        [backgroundTextField setTextAlignment:NSTextAlignmentCenter];
+        [backgroundTextField setDelegate:self];
+        [backgroundTextField setTag:10];
+        
+        inputAccView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, screenWidth, accessoryHeight)];
+        [inputAccView setBackgroundColor:accessoryBackgroundColor];
         [inputAccView addSubview:keyboardTitle];
         [inputAccView addSubview:backgroundTextField];
-        [inputAccView addSubview:verboseOutput];
         [[NSNotificationCenter defaultCenter] addObserver: self
                                                  selector: @selector(showKeyboard:)
                                                      name: @"Input.OnInputRequested"
@@ -89,20 +82,26 @@
 
 #pragma mark - keyboard
 
--(void) hideKeyboard:(id)sender{
+- (BOOL)canBecomeFirstResponder {
+    return NO;
+}
+
+-(void) hideKeyboard:(id)sender {
+    [backgroundTextField resignFirstResponder];
+    backgroundTextField.text = @"";
     [xbmcVirtualKeyboard resignFirstResponder];
 }
 
 -(void) showKeyboard:(NSNotification *)note{
     if ([AppDelegate instance].serverVersion == 11){
-        xbmcVirtualKeyboard.text = @" ";
+        backgroundTextField.text = @" ";
     }
     NSDictionary *params;
     if (note!=nil){
         params = [[note userInfo] objectForKey:@"params"];
     }
     keyboardTitle.text = @"";
-    xbmcVirtualKeyboard.keyboardType = UIKeyboardTypeDefault;
+    backgroundTextField.keyboardType = UIKeyboardTypeDefault;
     if (params != nil){
         if (((NSNull *)[params objectForKey:@"data"] != [NSNull null])){
             if (((NSNull *)[[params objectForKey:@"data"] objectForKey:@"title"] != [NSNull null])){
@@ -110,21 +109,22 @@
             }
             if (((NSNull *)[[params objectForKey:@"data"] objectForKey:@"value"] != [NSNull null])){
                 if (![[[params objectForKey:@"data"] objectForKey:@"value"] isEqualToString:@""]){
-                    xbmcVirtualKeyboard.text = [[params objectForKey:@"data"] objectForKey:@"value"];
+                    backgroundTextField.text = [[params objectForKey:@"data"] objectForKey:@"value"];
                 }
             }
             if (((NSNull *)[[params objectForKey:@"data"] objectForKey:@"type"] != [NSNull null])){
                 if ([[[params objectForKey:@"data"] objectForKey:@"type"] isEqualToString:@"number"]){
-                    xbmcVirtualKeyboard.keyboardType = UIKeyboardTypeNumbersAndPunctuation;
+                    backgroundTextField.keyboardType = UIKeyboardTypeNumbersAndPunctuation;
                 }
             }
         }
     }
     [xbmcVirtualKeyboard becomeFirstResponder];
+    [backgroundTextField becomeFirstResponder];
 }
 
 -(void)toggleVirtualKeyboard:(id)sender{
-    if ([xbmcVirtualKeyboard isFirstResponder]){
+    if ([xbmcVirtualKeyboard isFirstResponder] || [backgroundTextField isFirstResponder]){
         [self hideKeyboard:nil];
     }
     else {
@@ -134,22 +134,24 @@
 
 #pragma mark - UITextFieldDelegate Methods
 
-
 -(void)textFieldDidBeginEditing:(UITextField *)textField{
-    verboseOutput.text = xbmcVirtualKeyboard.text;
     float finalHeight = accessoryHeight - alignBottom;
+    CGRect screenBound = [[UIScreen mainScreen] bounds];
+    CGSize screenSize = screenBound.size;
+    screenWidth = screenSize.width;
+    CGRect frame = inputAccView.frame;
+    frame.size.width = screenWidth;
+    [inputAccView setFrame:frame];
+    
     if ([keyboardTitle.text isEqualToString:@""]){
         [inputAccView setFrame:
          CGRectMake(0, 0, screenWidth, finalHeight)];
-        [verboseOutput setFrame:
-         CGRectMake(padding, (int)(accessoryHeight/2) - (int)(verboseHeight/2) - (int)(alignBottom/2), screenWidth - padding * 2, verboseHeight)];
         [backgroundTextField setFrame:
          CGRectMake(padding - background_padding, (int)(accessoryHeight/2) - (int)(verboseHeight/2) - (int)(alignBottom/2), screenWidth - (padding - background_padding) * 2, verboseHeight)];
     }
     else{
         finalHeight = accessoryHeight;
         [inputAccView setFrame:CGRectMake(0, 0, screenWidth, finalHeight)];
-        [verboseOutput setFrame:CGRectMake(padding, (int)(accessoryHeight/2) - (int)(verboseHeight/2) + alignBottom, screenWidth - padding * 2, verboseHeight)];
         [backgroundTextField setFrame:CGRectMake(padding - background_padding, (int)(accessoryHeight/2) - (int)(verboseHeight/2) + alignBottom, screenWidth - (padding - background_padding) * 2, verboseHeight)];
     }
     [textField setInputAccessoryView:inputAccView];
@@ -160,39 +162,30 @@
 }
 
 -(BOOL) textField: (UITextField *)theTextField shouldChangeCharactersInRange: (NSRange)range replacementString: (NSString *)string {
-    if ([AppDelegate instance].serverVersion == 11){
+    if ([AppDelegate instance].serverVersion == 11) {
         if (range.location == 0){ //BACKSPACE
             [self sendXbmcHttp:@"SendKey(0xf108)"];
-            if ([verboseOutput.text length]>0){
-                [verboseOutput setText:[NSString stringWithFormat:@"%@", [verboseOutput.text substringToIndex:[verboseOutput.text length] - 1]]];
-            }
-            else{
-                verboseOutput.text = @"";
-            }
         }
         else{ // CHARACTER
             int x = (unichar) [string characterAtIndex: 0];
             if (x==10) {
                 [self GUIAction:@"Input.Select" params:[NSDictionary dictionary] httpAPIcallback:nil];
+                [backgroundTextField resignFirstResponder];
                 [xbmcVirtualKeyboard resignFirstResponder];
             }
             else if (x<1000){
                 [self sendXbmcHttp:[NSString stringWithFormat:@"SendKey(0xf1%x)", x]];
             }
-            [verboseOutput setText:[NSString stringWithFormat:@"%@%@", verboseOutput.text == nil ? @"" : verboseOutput.text, string]];
         }
         return NO;
     }
-    else{
+    else {
         NSString *stringToSend = [theTextField.text stringByReplacingCharactersInRange:range withString:string];
-//        if ([stringToSend isEqualToString:@""]){
-//            stringToSend = @"";
-//        }
-        verboseOutput.text = stringToSend;
-        if ([string length] != 0){
+        if ([string length] != 0) {
             int x = (unichar) [string characterAtIndex: 0];
-            if (x==10) {
+            if (x == 10) {
                 [self GUIAction:@"Input.SendText" params:[NSDictionary dictionaryWithObjectsAndKeys:[stringToSend substringToIndex:[stringToSend length] - 1], @"text", [NSNumber numberWithBool:TRUE], @"done", nil] httpAPIcallback:nil];
+                [backgroundTextField resignFirstResponder];
                 [xbmcVirtualKeyboard resignFirstResponder];
                 theTextField.text = @"";
                 return YES;
@@ -203,18 +196,19 @@
     }
 }
 
+-(void)textFieldDidEndEditing:(UITextField *)textField {
+    if (textField.tag == 10) {
+        [self performSelectorOnMainThread:@selector(hideKeyboard:) withObject:nil waitUntilDone:FALSE];
+    }
+}
+
 #pragma mark - json commands
 
 -(void)GUIAction:(NSString *)action params:(NSDictionary *)params httpAPIcallback:(NSString *)callback{
     jsonRPC = nil;
     jsonRPC = [[DSJSONRPC alloc] initWithServiceEndpoint:[AppDelegate instance].getServerJSONEndPoint andHTTPHeaders:[AppDelegate instance].getServerHTTPHeaders];
     [jsonRPC callMethod:action withParameters:params onCompletion:^(NSString *methodName, NSInteger callId, id methodResult, DSJSONRPCError *methodError, NSError* error) {
-        //        NSLog(@"Action %@ ok with %@ ", action , methodResult);
-        //        if (methodError!=nil || error != nil){
-        //            NSLog(@"method error %@", methodError);
-        //        }
         if ((methodError!=nil || error != nil) && callback!=nil){ // Backward compatibility
-            //            NSLog(@"method error %@", methodError);
             [self sendXbmcHttp:callback];
         }
     }];
