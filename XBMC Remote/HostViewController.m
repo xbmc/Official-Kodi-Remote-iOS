69db3654a14e0c9c8e3226227313a6bbd0c7d6c3
wutschel <andreebuschmann@t-online.de>
2021-01-20 20:49:37 +0100

    Hide HostVC tip text when editing in iPad Landscape mode

--- XBMC Remote/HostViewController.m
+++ XBMC Remote/HostViewController.m
@@ -151,10 +151,28 @@
     [[AppDelegate instance] saveServerList];
     [self.navigationController popViewControllerAnimated:YES];
 }
+
+#pragma mark - Helper
+
+- (void)tailorViewContent:(BOOL)isEditing {
+    if (isEditing) {
+        if (([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) && UIInterfaceOrientationIsLandscape([[UIApplication sharedApplication] statusBarOrientation])) {
+            tipView.hidden = YES;
+        }
+        else {
+            tipView.hidden = NO;
+        }
+    }
+    else {
+        tipView.hidden = NO;
+    }
+}
+
 #pragma mark - UITextFieldDelegate Methods
 
 - (void)textFieldDidBeginEditing:(UITextField *)textField{
     [textField setTextColor:[Utilities get1stLabelColor]];
+    [self tailorViewContent:YES];
 }
 -(void)resignKeyboard{
     [descriptionUI resignFirstResponder];
@@ -169,6 +187,7 @@
     [mac_4_UI resignFirstResponder];
     [mac_5_UI resignFirstResponder];
     [passwordUI resignFirstResponder];
+    [self tailorViewContent:NO];
 }
 
 -(BOOL)textFieldShouldReturn:(UITextField *)theTextField {
