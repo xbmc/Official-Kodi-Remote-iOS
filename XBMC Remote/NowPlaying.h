2660e15df836476268e54b3e281eb2ee306f28ac
wutschel <andreebuschmann@t-online.de>
2021-02-06 15:05:51 +0100

    Replace deprecated UIActionSheet in NowPlaying

--- XBMC Remote/NowPlaying.h
+++ XBMC Remote/NowPlaying.h
@@ -17,7 +17,7 @@
 @class RemoteController;
 @class DetailViewController;
 
-@interface NowPlaying : UIViewController <UITableViewDataSource, UITableViewDelegate, UIActionSheetDelegate, SDWebImageManagerDelegate, UIGestureRecognizerDelegate>{
+@interface NowPlaying : UIViewController <UITableViewDataSource, UITableViewDelegate, SDWebImageManagerDelegate, UIGestureRecognizerDelegate>{
     DSJSONRPC *jsonRPC;
     IBOutlet UITableView *playlistTableView;
     IBOutlet UITableViewCell *playlistTableViewCell;
