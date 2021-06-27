e6510980c1b07a32fd912e3a77ec242df356a6e6
wutschel <andreebuschmann@t-online.de>
2021-06-27 15:57:43 +0200

    Align indentations for ECSlidingViewController

--- XBMC Remote/ECSlidingViewController/UIImage+ImageWithUIView.m
+++ XBMC Remote/ECSlidingViewController/UIImage+ImageWithUIView.m
@@ -9,14 +9,14 @@
 #pragma mark TakeScreenShot
 
 + (UIImage*)imageWithUIView:(UIView*)view {
-  CGSize screenShotSize = view.bounds.size;
-  UIImage *img;  
-  UIGraphicsBeginImageContext(screenShotSize);
-  CGContextRef ctx = UIGraphicsGetCurrentContext();
-  [view drawLayer:view.layer inContext:ctx];
-  img = UIGraphicsGetImageFromCurrentImageContext();
-  UIGraphicsEndImageContext();
-  
-  return img;
+    CGSize screenShotSize = view.bounds.size;
+    UIImage *img;
+    UIGraphicsBeginImageContext(screenShotSize);
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    [view drawLayer:view.layer inContext:ctx];
+    img = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+
+    return img;
 }
 @end
