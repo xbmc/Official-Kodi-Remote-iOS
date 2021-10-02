4e1c7441a40cca45d15ac8230b4fa930c6c156b7
wutschel <andreebuschmann@t-online.de>
2021-10-02 18:36:06 +0200

    Send XBMC http commands asynchronously

--- XBMC Remote/Utilities.m
+++ XBMC Remote/Utilities.m
@@ -823,10 +823,8 @@
 + (void)sendXbmcHttp:(NSString*)command {
     GlobalData *obj = [GlobalData getInstance];
     NSString *userPassword = [obj.serverPass isEqualToString:@""] ? @"" : [NSString stringWithFormat:@":%@", obj.serverPass];
-
     NSString *serverHTTP = [NSString stringWithFormat:@"http://%@%@@%@:%@/xbmcCmds/xbmcHttp?command=%@", obj.serverUser, userPassword, obj.serverIP, obj.serverPort, command];
-    NSURL *url = [NSURL URLWithString:serverHTTP];
-    [NSString stringWithContentsOfURL:url encoding:NSUTF8StringEncoding error:NULL];
+    [[NSURLSession.sharedSession dataTaskWithURL:[NSURL URLWithString:serverHTTP]] resume];
 }
 
 @end
