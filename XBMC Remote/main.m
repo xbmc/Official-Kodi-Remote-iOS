f15a14e67bdff1234a6283c4c1b440332971a0ba
wutschel <andreebuschmann@t-online.de>
2021-11-01 11:54:14 +0100

    Remove Kodi Remote controlled language settings

--- XBMC Remote/main.m
+++ XBMC Remote/main.m
@@ -12,13 +12,6 @@
 
 int main(int argc, char *argv[]) {
     @autoreleasepool {
-        NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
-        if ([[userDefaults objectForKey:@"lang_preference"] length]) {
-            [userDefaults setObject:[NSArray arrayWithObjects:[userDefaults objectForKey:@"lang_preference"], nil] forKey:@"AppleLanguages"];
-        }
-        else {
-            [userDefaults removeObjectForKey:@"AppleLanguages"];
-        }
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
