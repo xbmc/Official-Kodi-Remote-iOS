75dc4aadb2593f8f91ab29c8da281764ffdf4a10
wutschel <andreebuschmann@t-online.de>
2022-01-22 10:28:13 +0100

    Add a remote button to the toolbar
    - Right of play control and left of volume button

--- XBMC Remote/ViewControllerIPad.h
+++ XBMC Remote/ViewControllerIPad.h
@@ -29,6 +29,7 @@
 	StackScrollViewController* stackScrollViewController;
     NSIndexPath *storeServerSelection;
     int YPOS;
+    UIButton *remoteButton;
     UIButton *xbmcLogo;
     UIButton *xbmcInfo;
     UIButton *powerButton;
