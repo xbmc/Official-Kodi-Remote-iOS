a0f009e7d49aa9421e41221ff6e02ed9337ba169
wutschel <andreebuschmann@t-online.de>
2022-02-18 07:18:06 +0100

    Match MD5 implementation to SHA256

--- XBMC Remote/NSString+MD5/NSString+MD5.m
+++ XBMC Remote/NSString+MD5/NSString+MD5.m
@@ -12,17 +12,15 @@
 @implementation NSString (MD5)
 
 - (NSString*)MD5String {
-    const char *cstr = [self UTF8String];
-    unsigned char result[16];
-    CC_MD5(cstr, (int)strlen(cstr), result);
+    const char *utf8chars = [self UTF8String];
+    unsigned char result[CC_MD5_DIGEST_LENGTH];
+    CC_MD5(utf8chars, (CC_LONG)strlen(utf8chars), result);
     
-    return [NSString stringWithFormat:
-            @"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
-            result[0], result[1], result[2], result[3],
-            result[4], result[5], result[6], result[7],
-            result[8], result[9], result[10], result[11],
-            result[12], result[13], result[14], result[15]
-            ];
+    NSMutableString *ret = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    for (int i = 0; i < CC_MD5_DIGEST_LENGTH; i++) {
+        [ret appendFormat:@"%02x", result[i]];
+    }
+    return ret;
 }
 
 - (NSString*)SHA256String {
