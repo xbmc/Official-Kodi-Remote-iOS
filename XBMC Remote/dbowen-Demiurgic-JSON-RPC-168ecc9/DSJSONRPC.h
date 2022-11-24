f88470cd3503f07a55d204339b4aa06381c17d00
wutschel <andreebuschmann@t-online.de>
2022-11-24 07:37:13 +0100

    Migrate DSJSONRPC to NSURLSession
    Use NSURDataSession delegate to be able to handle authentication challenge.

--- XBMC Remote/dbowen-Demiurgic-JSON-RPC-168ecc9/DSJSONRPC.h
+++ XBMC Remote/dbowen-Demiurgic-JSON-RPC-168ecc9/DSJSONRPC.h
@@ -53,39 +53,6 @@ typedef enum {
 } DSJSONRPCErrorType;
 
 /**
- *  Delegate used to provide information regarding web service calls made.
- *
-**/
-@protocol DSJSONRPCDelegate <NSObject>
-@optional
-/**
- *  Invoked upon the method successfully completing without the error key being set in the response.
- *
- *  methodResult will be the appropriate Objective-C object type based on the type set as the result on the server.
- *
-**/
-- (void)jsonRPC:(DSJSONRPC*)jsonRPC didFinishMethod:(NSString*)methodName forId:(NSInteger)aId withResult:(id)methodResult;
-
-/**
- *  Invoked when the method is completed and the error key is set in the response.
- *
- *  methodError is an Objective-C object which contains all information provided by the offical JSON-RPC error response structure.
- *
-**/
-- (void)jsonRPC:(DSJSONRPC*)jsonRPC didFinishMethod:(NSStream*)methodName forId:(NSInteger)aId withError:(DSJSONRPCError*)methodError;
-
-/**
- *  Invoked when an error occurs with the connection or when the JSON payload can't be (de)serialized.
- *
- *  The error number will be set to a value defined by DSJSONRPCError. 
- *  localizedDescription is the value from the original error that was generated.
- *
-**/
-- (void)jsonRPC:(DSJSONRPC*)jsonRPC didFailMethod:(NSString*)methodName forId:(NSInteger)aId withError:(NSError*)error;
-@end
-
-
-/**
  *  Invoked when and error occurs or upon method completion.
  *
  *  If methodError is set, the error occured on the server.
@@ -98,12 +65,9 @@ typedef enum {
 **/
 typedef void (^DSJSONRPCCompletionHandler)(NSString *methodName, NSInteger callId, id methodResult, DSJSONRPCError *methodError, NSError *internalError);
 
-
-@interface DSJSONRPC : NSObject <NSURLConnectionDataDelegate> {
+@interface DSJSONRPC : NSObject <NSURLSessionDataDelegate> {
     NSTimer* timer;
 }
-
-@property (nonatomic, DS_WEAK) id<DSJSONRPCDelegate> delegate;
 
 - (id)initWithServiceEndpoint:(NSURL*)serviceEndpoint;
 - (id)initWithServiceEndpoint:(NSURL*)serviceEndpoint andHTTPHeaders:(NSDictionary*)httpHeaders;
