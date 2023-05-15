44ace043a02f45be0ded45b8e2c558c31a429adc
wutschel <andreebuschmann@t-online.de>
2023-05-15 10:04:32 +0200

    Fix API request configuration for TV Shows
    Restore providing "episodeid" to fix problems with "episodeid" related functionality (play, resume, queue, episode details).
    Read "title" in extra_info_parameters and extra_section_parameters properties and use this is main identifier ("row1").

--- XBMC Remote/DetailViewController.m
+++ XBMC Remote/DetailViewController.m
@@ -2485,7 +2485,7 @@
         title.text = [NSString stringWithFormat:@"%@. %@", item[@"channelnumber"], item[@"label"]];
     }
     else if (item[@"episodeid"]) {
-        title.text = [Utilities formatTVShowStringForSeason:item[@"season"] episode:item[@"episode"] title:item[@"title"]];
+        title.text = [Utilities formatTVShowStringForSeason:item[@"season"] episode:item[@"episode"] title:item[@"label"]];
     }
     else {
         title.text = item[@"label"];
