639a79433cc323f35d6900afa97b8488c2193f36
wutschel <andreebuschmann@t-online.de>
2024-04-02 19:16:09 +0200

    Update network files from Apple Open Source project Darwin
    #head on April 2nd, 2024.

--- XBMC Remote/Network/if_ether.h
+++ XBMC Remote/Network/if_ether.h
@@ -2,7 +2,7 @@
  * Copyright (c) 2000-2011 Apple Inc. All rights reserved.
  *
  * @APPLE_OSREFERENCE_LICENSE_HEADER_START@
- * 
+ *
  * This file contains Original Code and/or Modifications of Original Code
  * as defined in and that are subject to the Apple Public Source License
  * Version 2.0 (the 'License'). You may not use this file except in
@@ -11,10 +11,10 @@
  * unlawful or unlicensed copies of an Apple operating system, or to
  * circumvent, violate, or enable the circumvention or violation of, any
  * terms of an Apple operating system software license agreement.
- * 
+ *
  * Please obtain a copy of the License at
  * http://www.opensource.apple.com/apsl/ and read it before using this file.
- * 
+ *
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -22,12 +22,12 @@
  * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
  * Please see the License for the specific language governing rights and
  * limitations under the License.
- * 
+ *
  * @APPLE_OSREFERENCE_LICENSE_HEADER_END@
  */
 /*
  * Copyright (c) 1982, 1986, 1993
- *	The Regents of the University of California.  All rights reserved.
+ *    The Regents of the University of California.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -39,8 +39,8 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
- *	This product includes software developed by the University of
- *	California, Berkeley and its contributors.
+ *    This product includes software developed by the University of
+ *    California, Berkeley and its contributors.
  * 4. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
@@ -57,7 +57,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- *	@(#)if_ether.h	8.3 (Berkeley) 5/2/95
+ *    @(#)if_ether.h    8.3 (Berkeley) 5/2/95
  * $FreeBSD: src/sys/netinet/if_ether.h,v 1.24 1999/12/29 04:40:58 peter Exp $
  */
 
@@ -69,7 +69,7 @@
 
 #include <netinet/in.h>
 #include "if_arp.h"
-#define ea_byte	ether_addr_octet
+#define ea_byte ether_addr_octet
 
 /*
  * Macro to map an IP multicast address to an Ethernet multicast address.
@@ -77,31 +77,31 @@
  * and the low-order 23 bits are taken from the low end of the IP address.
  */
 #define ETHER_MAP_IP_MULTICAST(ipaddr, enaddr) \
-	/* struct in_addr *ipaddr; */ \
-	/* u_char enaddr[ETHER_ADDR_LEN];	   */ \
+    /* struct in_addr *ipaddr; */ \
+    /* u_char enaddr[ETHER_ADDR_LEN];       */ \
 { \
-	(enaddr)[0] = 0x01; \
-	(enaddr)[1] = 0x00; \
-	(enaddr)[2] = 0x5e; \
-	(enaddr)[3] = ((const u_char*)ipaddr)[1] & 0x7f; \
-	(enaddr)[4] = ((const u_char*)ipaddr)[2]; \
-	(enaddr)[5] = ((const u_char*)ipaddr)[3]; \
+    (enaddr)[0] = 0x01; \
+    (enaddr)[1] = 0x00; \
+    (enaddr)[2] = 0x5e; \
+    (enaddr)[3] = ((const u_char *)ipaddr)[1] & 0x7f; \
+    (enaddr)[4] = ((const u_char *)ipaddr)[2]; \
+    (enaddr)[5] = ((const u_char *)ipaddr)[3]; \
 }
 /*
  * Macro to map an IP6 multicast address to an Ethernet multicast address.
  * The high-order 16 bits of the Ethernet address are statically assigned,
  * and the low-order 32 bits are taken from the low end of the IP6 address.
  */
-#define ETHER_MAP_IPV6_MULTICAST(ip6addr, enaddr)			\
-/* struct	in6_addr *ip6addr; */					\
-/* u_char	enaddr[ETHER_ADDR_LEN]; */				\
+#define ETHER_MAP_IPV6_MULTICAST(ip6addr, enaddr)                       \
+/* struct    in6_addr *ip6addr; */                                   \
+/* u_char    enaddr[ETHER_ADDR_LEN]; */                              \
 {                                                                       \
-	(enaddr)[0] = 0x33;						\
-	(enaddr)[1] = 0x33;						\
-	(enaddr)[2] = ((const u_char*)ip6addr)[12];				\
-	(enaddr)[3] = ((const u_char*)ip6addr)[13];				\
-	(enaddr)[4] = ((const u_char*)ip6addr)[14];				\
-	(enaddr)[5] = ((const u_char*)ip6addr)[15];				\
+    (enaddr)[0] = 0x33;                                             \
+    (enaddr)[1] = 0x33;                                             \
+    (enaddr)[2] = ((const u_char *)ip6addr)[12];                            \
+    (enaddr)[3] = ((const u_char *)ip6addr)[13];                            \
+    (enaddr)[4] = ((const u_char *)ip6addr)[14];                            \
+    (enaddr)[5] = ((const u_char *)ip6addr)[15];                            \
 }
 
 /*
@@ -111,35 +111,44 @@
  * to resolving internet addresses.  Field names used correspond to
  * RFC 826.
  */
-struct	ether_arp {
-	struct	arphdr ea_hdr;	/* fixed-size header */
-	u_char	arp_sha[ETHER_ADDR_LEN];	/* sender hardware address */
-	u_char	arp_spa[4];	/* sender protocol address */
-	u_char	arp_tha[ETHER_ADDR_LEN];	/* target hardware address */
-	u_char	arp_tpa[4];	/* target protocol address */
+struct  ether_arp {
+    struct  arphdr ea_hdr;  /* fixed-size header */
+    u_char  arp_sha[ETHER_ADDR_LEN];        /* sender hardware address */
+    u_char  arp_spa[4];     /* sender protocol address */
+    u_char  arp_tha[ETHER_ADDR_LEN];        /* target hardware address */
+    u_char  arp_tpa[4];     /* target protocol address */
 };
-#define	arp_hrd	ea_hdr.ar_hrd
-#define	arp_pro	ea_hdr.ar_pro
-#define	arp_hln	ea_hdr.ar_hln
-#define	arp_pln	ea_hdr.ar_pln
-#define	arp_op	ea_hdr.ar_op
+#define arp_hrd ea_hdr.ar_hrd
+#define arp_pro ea_hdr.ar_pro
+#define arp_hln ea_hdr.ar_hln
+#define arp_pln ea_hdr.ar_pln
+#define arp_op  ea_hdr.ar_op
 
 struct sockaddr_inarp {
-	u_char	sin_len;
-	u_char	sin_family;
-	u_short sin_port;
-	struct	in_addr sin_addr;
-	struct	in_addr sin_srcaddr;
-	u_short	sin_tos;
-	u_short	sin_other;
-#define	SIN_PROXY	0x1
-#define	SIN_ROUTER	0x2
+    u_char  sin_len;
+    u_char  sin_family;
+    u_short sin_port;
+    struct  in_addr sin_addr;
+    struct  in_addr sin_srcaddr;
+    u_short sin_tos;
+    u_short sin_other;
+#define SIN_PROXY       0x1
+#define SIN_ROUTER      0x2
 };
 /*
  * IP and ethernet specific routing flags
  */
-#define	RTF_USETRAILERS	RTF_PROTO1	/* use trailers */
-#define RTF_ANNOUNCE	RTF_PROTO2	/* announce new arp entry */
+#define RTF_USETRAILERS RTF_PROTO1      /* use trailers */
+#define RTF_ANNOUNCE    RTF_PROTO2      /* announce new arp entry */
 
+#ifdef BSD_KERNEL_PRIVATE
+extern u_char   ether_ipmulticast_min[ETHER_ADDR_LEN];
+extern u_char   ether_ipmulticast_max[ETHER_ADDR_LEN];
+extern struct   ifqueue arpintrq;
+
+int     arpresolve(struct ifnet *, struct rtentry *, struct mbuf *,
+    struct sockaddr *, u_char *, struct rtentry *);
+void    arp_ifinit(struct ifnet *, struct ifaddr *);
+#endif /* BSD_KERNEL_PRIVATE */
 
 #endif /* _NETINET_IF_ETHER_H_ */
