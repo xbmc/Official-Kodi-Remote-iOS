50649eb5c3be6dd3aa3e05170bfcbeb8176a2906
wutschel <andreebuschmann@t-online.de>
2024-04-21 15:18:18 +0200

    fixup: collection view init

--- XBMC Remote/DetailViewController.m
+++ XBMC Remote/DetailViewController.m
@@ -5551,6 +5551,7 @@
     else {
         self.navigationController.navigationBar.tintColor = ICON_TINT_COLOR;
     }
+    [self displayData];
     if (channelListView || channelGuideView) {
         [channelListUpdateTimer invalidate];
         // Set up a timer that will always trigger at the start of each local minute. This supports
