caf91f850f91c2f2e77320c9edf4dacf0643fc1d
wutschel <andreebuschmann@t-online.de>
2024-09-30 08:56:34 +0200

    Load profile on select
    Only load non-active profile and request password prompt.

--- XBMC Remote/DetailViewController.m
+++ XBMC Remote/DetailViewController.m
@@ -1597,6 +1597,9 @@
     else if ([parameters[@"forcePlayback"] boolValue]) {
         [self startPlayback:item indexPath:indexPath shuffle:NO];
     }
+    else if ([item[@"family"] isEqualToString:@"profile"]) {
+        [self loadProfile:item];
+    }
     else if (methods[@"method"] != nil && ![parameters[@"forceActionSheet"] boolValue] && !stackscrollFullscreen) {
         // There is a child and we want to show it (only when not in fullscreen)
         [self viewChild:indexPath item:item displayPoint:point];
@@ -4420,6 +4423,26 @@
         }
         else {
             [Utilities showMessage:failureMessage color:ERROR_MESSAGE_COLOR];
+        }
+    }];
+}
+
+- (void)loadProfile:(NSDictionary*)item {
+    NSString *profileName = item[@"label"];
+    if ([profileName isEqualToString:AppDelegate.instance.currentProfile]) {
+        return;
+    }
+    // Load user profile
+    [[Utilities getJsonRPC]
+     callMethod:@"Profiles.LoadProfile"
+     withParameters:@{
+        @"profile": profileName,
+        @"prompt": @YES,
+    }
+     onCompletion:^(NSString *methodName, NSInteger callId, id methodResult, DSJSONRPCError *methodError, NSError *error) {
+        if (!error && !methodError) {
+            AppDelegate.instance.currentProfile = profileName;
+            [dataList reloadData];
         }
     }];
 }
