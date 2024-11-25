9e35748fa0aef123dc64438a4a6b104e362381af
wutschel <andreebuschmann@t-online.de>
2024-11-25 06:27:07 +0100

    Use nil instead of empty callback

--- XBMC Remote/DetailViewController.m
+++ XBMC Remote/DetailViewController.m
@@ -5857,7 +5857,7 @@
         }
         activeLayoutView.contentOffset = CGPointMake(0, iOSYDelta);
     }
-                                 completion:^(id<UIViewControllerTransitionCoordinatorContext> context) {}];
+                                 completion:nil];
 }
 
 - (void)updateSearchResultsForSearchController:(UISearchController*)searchController {
