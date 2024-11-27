0744ddeab327603b8afe2ff817cffe34d5a1fecb
wutschel <andreebuschmann@t-online.de>
2024-11-27 06:37:55 +0100

    Remove obsolete assignment to dataList.backgroundView

--- XBMC Remote/DetailViewController.m
+++ XBMC Remote/DetailViewController.m
@@ -5924,7 +5924,6 @@
 
     [button7 addTarget:self action:@selector(handleChangeSortLibrary) forControlEvents:UIControlEventTouchUpInside];
     self.edgesForExtendedLayout = UIRectEdgeNone;
-    dataList.backgroundView = [[UIView alloc] initWithFrame:CGRectZero];
     dataList.indicatorStyle = UIScrollViewIndicatorStyleDefault;
     
     CGRect frame = dataList.frame;
