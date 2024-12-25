96e981f087a1e1fb3d9141a8f2d505e24be3fc7b
wutschel <andreebuschmann@t-online.de>
2024-12-25 15:23:57 +0100

    Call startAnimating after early return to save a line of code

--- XBMC Remote/DetailViewController.m
+++ XBMC Remote/DetailViewController.m
@@ -4055,8 +4055,6 @@
 }
 
 - (void)addQueue:(NSDictionary*)item indexPath:(NSIndexPath*)indexPath afterCurrentItem:(BOOL)afterCurrent {
-    UIActivityIndicatorView *cellActivityIndicator = [self getCellActivityIndicator:indexPath];
-    [cellActivityIndicator startAnimating];
     mainMenu *menuItem = [self getMainMenu:item];
     int activeTab = [self getActiveTab:item];
     NSDictionary *mainFields = menuItem.mainFields[activeTab];
@@ -4067,10 +4065,11 @@
     int playlistid = [mainFields[@"playlistid"] intValue];
     id playlistItems = [self buildPlaylistItems:item key:mainFields[@"row9"]];
     if (!playlistItems) {
-        [cellActivityIndicator stopAnimating];
         [Utilities showMessage:LOCALIZED_STR(@"Cannot do that") color:ERROR_MESSAGE_COLOR];
         return;
     }
+    UIActivityIndicatorView *cellActivityIndicator = [self getCellActivityIndicator:indexPath];
+    [cellActivityIndicator startAnimating];
     NSDictionary *playlistParams = @{
         @"playlistid": @(playlistid),
         @"item": playlistItems,
@@ -4207,8 +4206,6 @@
     if (mainFields.count == 0) {
         return;
     }
-    UIActivityIndicatorView *cellActivityIndicator = [self getCellActivityIndicator:indexPath];
-    [cellActivityIndicator startAnimating];
     id optionsKey;
     id optionsValue;
     if (AppDelegate.instance.serverVersion > 11) {
@@ -4220,10 +4217,11 @@
     }
     id playlistItems = [self buildPlaylistItems:item key:mainFields[@"row9"]];
     if (!playlistItems) {
-        [cellActivityIndicator stopAnimating];
         [Utilities showMessage:LOCALIZED_STR(@"Cannot do that") color:ERROR_MESSAGE_COLOR];
         return;
     }
+    UIActivityIndicatorView *cellActivityIndicator = [self getCellActivityIndicator:indexPath];
+    [cellActivityIndicator startAnimating];
     NSDictionary *playbackParams = [NSDictionary dictionaryWithObjectsAndKeys:
                                     playlistItems, @"item",
                                     optionsValue, optionsKey,
@@ -4248,8 +4246,6 @@
     if (mainFields.count == 0) {
         return;
     }
-    UIActivityIndicatorView *cellActivityIndicator = [self getCellActivityIndicator:indexPath];
-    [cellActivityIndicator startAnimating];
     
     NSString *key = mainFields[@"row8"];
     id value = item[key];
@@ -4257,10 +4253,11 @@
         key = @"directory";
     }
     if (!value || !key) {
-        [cellActivityIndicator stopAnimating];
         [Utilities showMessage:LOCALIZED_STR(@"Cannot do that") color:ERROR_MESSAGE_COLOR];
         return;
     }
+    UIActivityIndicatorView *cellActivityIndicator = [self getCellActivityIndicator:indexPath];
+    [cellActivityIndicator startAnimating];
     NSDictionary *playbackParams = @{@"item": @{key: value}};
     
     // Usually we just send key:value as this fits the common use cases. But for picture folders we must
