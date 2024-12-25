3c8f4d9273dbcac5a8ebd6cedb16be24dbbb8ca8
wutschel <andreebuschmann@t-online.de>
2024-12-25 16:37:53 +0100

    Remove enabling/disabling menu button

--- XBMC Remote/ShowInfoViewController.m
+++ XBMC Remote/ShowInfoViewController.m
@@ -466,14 +466,12 @@
             parameterName = @"broadcastid";
         }
     }
-    self.navigationItem.rightBarButtonItem.enabled = NO;
     [activityIndicatorView startAnimating];
     NSDictionary *parameters = @{parameterName: itemid};
     [[Utilities getJsonRPC] callMethod:methodToCall
          withParameters:parameters
            onCompletion:^(NSString *methodName, NSInteger callId, id methodResult, DSJSONRPCError *methodError, NSError *error) {
                [activityIndicatorView stopAnimating];
-               self.navigationItem.rightBarButtonItem.enabled = YES;
                if (error == nil && methodError == nil) {
                    [self animateRecordAction];
                    NSNumber *status = @(![item[@"isrecording"] boolValue]);
@@ -515,7 +513,6 @@
     NowPlaying *nowPlaying = [[NowPlaying alloc] initWithNibName:@"NowPlaying" bundle:nil];
     nowPlaying.detailItem = self.detailItem;
     [self.navigationController pushViewController:nowPlaying animated:YES];
-    self.navigationItem.rightBarButtonItem.enabled = YES;
 }
 
 - (BOOL)enableJewelCases {
@@ -1657,7 +1654,6 @@
         [Utilities showMessage:LOCALIZED_STR(@"Cannot do that") color:ERROR_MESSAGE_COLOR];
         return;
     }
-    self.navigationItem.rightBarButtonItem.enabled = NO;
     [activityIndicatorView startAnimating];
     NSDictionary *playlistParams = @{
         @"playlistid": @(playlistid),
@@ -1687,7 +1683,6 @@
                                  [[NSNotificationCenter defaultCenter] postNotificationName:@"XBMCPlaylistHasChanged" object:nil];
                              }
                          }];
-                         self.navigationItem.rightBarButtonItem.enabled = YES;
                      }
                      else {
                          [self addToPlaylist:playlistParams];
@@ -1713,7 +1708,6 @@
         if (error == nil && methodError == nil) {
             [[NSNotificationCenter defaultCenter] postNotificationName: @"XBMCPlaylistHasChanged" object: nil];
         }
-        self.navigationItem.rightBarButtonItem.enabled = YES;
     }];
 }
 
@@ -1729,7 +1723,6 @@
         [Utilities showMessage:LOCALIZED_STR(@"Cannot do that") color:ERROR_MESSAGE_COLOR];
         return;
     }
-    self.navigationItem.rightBarButtonItem.enabled = NO;
     [activityIndicatorView startAnimating];
     NSDictionary *params = @{
         @"item": @{
@@ -1751,7 +1744,6 @@
             [self showNowPlaying];
             [Utilities checkForReviewRequest];
         }
-        self.navigationItem.rightBarButtonItem.enabled = YES;
     }];
 }
 
