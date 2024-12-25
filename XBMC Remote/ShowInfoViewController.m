5b08d1c03e96854a1fb8403a178d3417f8c5ae25
wutschel <andreebuschmann@t-online.de>
2024-12-25 14:51:31 +0100

    Call stopAnimating in error case

--- XBMC Remote/ShowInfoViewController.m
+++ XBMC Remote/ShowInfoViewController.m
@@ -1384,6 +1384,7 @@
                         completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *url) {
                         __auto_type strongSelf = weakSelf;
                         if (!strongSelf) {
+                            [activityIndicatorView stopAnimating];
                             return;
                         }
                         if (image != nil) {
