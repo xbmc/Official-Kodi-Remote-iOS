8515e453bac7cc16fdc81cf09af1a9e8f5044ab7
wutschel <andreebuschmann@t-online.de>
2025-05-24 10:39:10 +0200

    Migrate to UIWindowScene

---
+++ XBMC Remote/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Kodi Remote
+//
+//  Created by Buschmann on 24.05.25.
+//  Copyright © 2025 Team Kodi. All rights reserved.
+//
+
+@import UIKit;
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate> {}
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
