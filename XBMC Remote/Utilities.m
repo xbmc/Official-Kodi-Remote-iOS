e9031e99e5ad0989e9bc1956d38a1c7834f86b64
wutschel <andreebuschmann@t-online.de>
2025-05-24 22:52:18 +0200

    Move DidBecomeActive and WillEnterForeground to SceneDelegate

--- XBMC Remote/Utilities.m
+++ XBMC Remote/Utilities.m
@@ -1204,4 +1204,8 @@
     [[NSNotificationCenter defaultCenter] postNotificationName:@"XBMCServerHasChanged" object:nil];
 }
 
++ (void)setIdleTimerFromUserDefaults {
+    UIApplication.sharedApplication.idleTimerDisabled = [[NSUserDefaults standardUserDefaults] boolForKey:@"lockscreen_preference"];
+}
+
 @end
