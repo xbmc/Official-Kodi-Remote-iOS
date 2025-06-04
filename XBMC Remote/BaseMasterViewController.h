10e1704520880d0fc6dd7984848e54e77ab10788
wutschel <andreebuschmann@t-online.de>
2025-06-04 21:06:15 +0200

    Create BaseMasterViewController as base class

---
+++ XBMC Remote/BaseMasterViewController.h
@@ -0,0 +1,11 @@
+//
+//  BaseMasterViewController.h
+//  Kodi Remote
+//
+//  Created by Buschmann on 04.06.25.
+//  Copyright © 2025 Team Kodi. All rights reserved.
+//
+
+@interface BaseMasterViewController : UIViewController
+
+@end
---
+++ XBMC Remote/BaseMasterViewController.m
@@ -0,0 +1,15 @@
+//
+//  BaseMasterViewController.m
+//  Kodi Remote
+//
+//  Created by Buschmann on 04.06.25.
+//  Copyright © 2025 Team Kodi. All rights reserved.
+//
+
+#import "BaseMasterViewController.h"
+
+@import UIKit;
+
+@implementation BaseMasterViewController
+
+@end
