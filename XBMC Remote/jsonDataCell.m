b55069cd066817d0bde9bfea682b282c81c113f2
wutschel <andreebuschmann@t-online.de>
2025-10-09 08:53:38 +0200

    Remove unused lineSeparator from jsonDataCell

--- XBMC Remote/jsonDataCell.m
+++ XBMC Remote/jsonDataCell.m
@@ -4,7 +4,6 @@
 @implementation jsonDataCell
 
 @synthesize urlImageView;
-@synthesize lineSeparator;
 
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString*)reuseIdentifier {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
