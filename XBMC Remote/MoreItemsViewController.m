c3cd13c8b4af9f8511c39bff3314fff643ae8ce0
wutschel <andreebuschmann@t-online.de>
2025-12-07 15:57:57 +0100

    Introduce ICON_TINT_COLOR_DARK for dark icons is light mode

--- XBMC Remote/MoreItemsViewController.m
+++ XBMC Remote/MoreItemsViewController.m
@@ -77,7 +77,7 @@
                                               ICON_HEIGHT);
         UIImageView *iconImage = [[UIImageView alloc] initWithFrame:iconImageViewRect];
         UIImage *image = [Utilities setLightDarkModeImageAsset:[UIImage imageNamed:item[@"icon"]]
-                                                    lightColor:UIColor.darkGrayColor
+                                                    lightColor:ICON_TINT_COLOR_DARK
                                                      darkColor:ICON_TINT_COLOR];
         iconImage.image = image;
         [cell.contentView addSubview:iconImage];
