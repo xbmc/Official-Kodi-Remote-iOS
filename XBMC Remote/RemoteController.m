5a332ce1442bd1d86494725dff1e6896cc9ced34
wutschel <andreebuschmann@t-online.de>
2026-03-08 15:26:09 +0100

    Remote hidesSharedBackground

--- XBMC Remote/RemoteController.m
+++ XBMC Remote/RemoteController.m
@@ -995,10 +995,17 @@ static void *TorchRemoteContext = &TorchRemoteContext;
         // Add the power and custom button buttons to navigation bar
         UIImage *customImg = [UIImage imageNamed:@"icon_custom_buttons"];
         UIImage *powerImg = [UIImage imageNamed:@"icon_power"];
+        UIBarButtonItem *customButton = [[UIBarButtonItem alloc] initWithImage:customImg style:UIBarButtonItemStylePlain target:self action:@selector(enterCustomButtons)];
+        UIBarButtonItem *fixedSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:nil action:nil];
+        UIBarButtonItem *powerButton = [[UIBarButtonItem alloc] initWithImage:powerImg style:UIBarButtonItemStylePlain target:self action:@selector(powerControl)];
+        customButton.tintColor = powerButton.tintColor = ICON_TINT_COLOR;
+        if (@available(iOS 26.0, *)) {
+            customButton.hidesSharedBackground = powerButton.hidesSharedBackground = YES;
+        }
         self.navigationItem.rightBarButtonItems = @[
-            [[UIBarButtonItem alloc] initWithImage:customImg style:UIBarButtonItemStylePlain target:self action:@selector(enterCustomButtons)],
-            [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:nil action:nil],
-            [[UIBarButtonItem alloc] initWithImage:powerImg style:UIBarButtonItemStylePlain target:self action:@selector(powerControl)],
+            customButton,
+            fixedSpace,
+            powerButton,
         ];
         self.navigationController.navigationBar.barTintColor = REMOTE_CONTROL_BAR_TINT_COLOR;
         [self.navigationController setNavigationBarHidden:NO animated:YES];
