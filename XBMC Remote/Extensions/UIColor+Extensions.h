634a2f943bfd4807c499717924161a78173fb02d
wutschel <andreebuschmann@t-online.de>
2026-04-05 16:41:59 +0200

    Introduce UIColor extensions

---
+++ XBMC Remote/Extensions/UIColor+Extensions.h
@@ -0,0 +1,30 @@
+//
+//  UIColor+Extensions.h
+//  Kodi Remote
+//
+//  Created by Buschmann on 05.04.26.
+//  Copyright © 2026 Team Kodi. All rights reserved.
+//
+
+@import Foundation;
+@import UIKit;
+
+@interface UIColor (Extensions)
+
++ (UIColor*)getSystemRed:(CGFloat)alpha;
++ (UIColor*)getSystemGreen:(CGFloat)alpha;
++ (UIColor*)getKodiBlue;
++ (UIColor*)getSystemBlue;
++ (UIColor*)getSystemGray1;
++ (UIColor*)getSystemGray2;
++ (UIColor*)getSystemGray3;
++ (UIColor*)getSystemGray4;
++ (UIColor*)getSystemGray5;
++ (UIColor*)getSystemGray6;
++ (UIColor*)get1stLabelColor;
++ (UIColor*)get2ndLabelColor;
++ (UIColor*)get3rdLabelColor;
++ (UIColor*)get4thLabelColor;
++ (UIColor*)getGrayColor:(int)tone alpha:(CGFloat)alpha;
+
+@end
