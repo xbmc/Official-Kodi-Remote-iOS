485c3dd79a1745444812c50dfb2ccd6a9d2022b0
wutschel <andreebuschmann@t-online.de>
2026-04-25 10:13:19 +0200

    Move disableScrollsToTopPropertyOnAllSubviewsOf to BaseActionVC

--- XBMC Remote/DetailViewController.m
+++ XBMC Remote/DetailViewController.m
@@ -5344,15 +5344,6 @@
     }
 }
 
-- (void)disableScrollsToTopPropertyOnAllSubviewsOf:(UIView*)view {
-    for (UIView *subview in view.subviews) {
-        if ([subview isKindOfClass:[UIScrollView class]]) {
-            ((UIScrollView*)subview).scrollsToTop = NO;
-        }
-        [self disableScrollsToTopPropertyOnAllSubviewsOf:subview];
-    }
-}
-
 - (BOOL)collectionViewCanBeEnabled {
     MainMenu *menuItem = self.detailItem;
     NSDictionary *parameters = menuItem.mainParameters[chosenTab];
