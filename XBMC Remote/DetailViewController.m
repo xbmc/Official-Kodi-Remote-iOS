8c3f0b187c42c46f6994aa7f7d3690607f3ff925
wutschel <andreebuschmann@t-online.de>
2026-05-10 18:35:10 +0200

    fixup: scrollIndicatorInsets (2)

--- XBMC Remote/DetailViewController.m
+++ XBMC Remote/DetailViewController.m
@@ -5535,6 +5535,7 @@
     UIEdgeInsets viewInsets = dataList.contentInset;
     viewInsets.top = [self getTableInsetTop];
     dataList.contentInset = viewInsets;
+    dataList.scrollIndicatorInsets = viewInsets;
     dataList.indicatorStyle = UIScrollViewIndicatorStyleDefault;
     
     CGRect frame = maskView.frame;
