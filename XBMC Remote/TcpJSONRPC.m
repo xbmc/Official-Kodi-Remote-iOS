0cb1cf0011a2be27abbb7cd1798c26e60897d59f
wutschel <andreebuschmann@t-online.de>
2026-05-15 10:31:40 +0200

    Directly use NSInteger to avoid need for cast

--- XBMC Remote/TcpJSONRPC.m
+++ XBMC Remote/TcpJSONRPC.m
@@ -110,9 +110,9 @@ NSInputStream *inStream;
         case NSStreamEventHasBytesAvailable:
             if (theStream == inStream) {
                 uint8_t buffer[1024];
-                int len;
+                NSInteger len;
                 while ([inStream hasBytesAvailable]) {
-                    len = (int)[inStream read:buffer maxLength:sizeof(buffer)];
+                    len = [inStream read:buffer maxLength:sizeof(buffer)];
                     if (len > 0) {
                         NSData *output = [[NSData alloc] initWithBytes:buffer length:len];
                         if (nil != output) {
