b587a28cd0ce821242f3f78886ea00f490970cf3
wutschel <andreebuschmann@t-online.de>
2026-09-07 16:02:26 +0200

    Remove nop

--- XBMC Remote/DetailViewController.m
+++ XBMC Remote/DetailViewController.m
@@ -1292,7 +1292,6 @@
     channelListView = [methods[@"channelListView"] boolValue];
     recordingListView = [methods[@"recordingListView"] boolValue];
     timerListView = [methods[@"timerListView"] boolValue];
-    activeLayoutView.contentOffset = activeLayoutView.contentOffset;
     [self checkFullscreenButton:NO];
     NSDictionary *newParameters = [self addExtraProperties:parameters];
     if (!tvShowsBannerView) {
