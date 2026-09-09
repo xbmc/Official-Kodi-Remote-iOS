cc31120f1ddf85db8652570359614f19e5ea09f3
wutschel <andreebuschmann@t-online.de>
2026-09-09 19:47:23 +0200

    key and object for "row15" not needed
    For the parameters I am not aware of any use case where row15 object would be non-nil either.

--- XBMC Remote/NowPlaying.m
+++ XBMC Remote/NowPlaying.m
@@ -1970,10 +1970,6 @@
     if (methods[@"method"] != nil) { // THERE IS A CHILD
         NSDictionary *mainFields = menuItem.mainFields[activeTab];
         NSMutableDictionary *parameters = menuItem.subItem.mainParameters[activeTab];
-        NSString *key = @"null";
-        if (item[mainFields[@"row15"]] != nil) {
-            key = mainFields[@"row15"];
-        }
         id objKey = mainFields[@"row6"];
         id obj = [Utilities getNumberFromItem:item[objKey]];
         if (![parameters[@"disableFilterParameter"] boolValue]) {
@@ -1992,7 +1988,6 @@
                                             obj, objKey,
                                             parameters[@"parameters"][@"properties"], @"properties",
                                             parameters[@"parameters"][@"sort"], @"sort",
-                                            item[mainFields[@"row15"]], key,
                                             nil];
         
         menuItem.subItem.mainParameters[activeTab] = newMainParameters;
